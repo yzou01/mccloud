b75a63c2de395ea2b0492238da42173ebb83bf61
Qianmiao <pcd76o6BHumYPsPnDWq4>
2022-08-10 22:52:50 +1000

    Improve UI for Add Invoices

--- templates/Invoices/add.php
+++ templates/Invoices/add.php
@@ -5,41 +5,61 @@
  * @var \Cake\Collection\CollectionInterface|string[] $addCosts
  */
 ?>
+
 <body class="sb-nav-fixed">
 <?php echo $this->element('navbar/navbar')?>
 <div id="layoutSidenav">
     <?php echo $this->element('navbar/sidebar')?>
     <div id="layoutSidenav_content">
         <main>
+            <br><br>
             <div class=" card mb-4">
-                <div class="card-header  ">
-                    <i class="fas fa-table me-1"></i>
-                    <?= $this->Html->link(__('Invoices'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+                <div class="card-header">
+                    <i class="fas fa-table me-1" style="padding-top: 11px"></i>
+                    Add Invoices
+                    <?= $this->Html->link(__('List Invoices'), ['action' => 'index'], ['class' => 'btn btn-primary', 'style' => 'float: right; margin-right: 5px']) ?>
                 </div>
                 <div class="card-body">
-
                     <div class="column-responsive column-80">
-                        <div class="factories form content">
+                        <div class="skus form content">
                             <?= $this->Form->create($invoice) ?>
                             <fieldset>
-                                <legend><?= __('Add Invoice') ?></legend>
-                                <?php
-                                echo $this->Form->control('number');
-                                echo $this->Form->control('date');
-                                echo $this->Form->control('currency_of_origin');
-                                echo $this->Form->control('currency_rate');
-                                echo $this->Form->control('add_cost_id', ['options' => $addCosts]);
-                                ?>
+                                <legend><?= __('Add Invoices') ?></legend>
+                                <div class="form-label">
+                                    <?php
+                                    echo $this->Form->control('number',['class'=>'form-control']);
+                                    ?>
+                                </div>
+                                <div class="form-label">
+                                    <?php
+                                    echo $this->Form->control('date',['class'=>'form-control']);
+                                    ?>
+                                </div>
+                                <div class="form-label">
+                                    <?php
+                                    echo $this->Form->control('currency_of_origin',['class'=>'form-control']);
+                                    ?>
+                                </div>
+                                <div class="form-label">
+                                    <?php
+                                    echo $this->Form->control('currency_rate',['class'=>'form-control']);
+                                    ?>
+                                </div>
+                                <div class="form-label">
+                                    <?php
+                                    echo $this->Form->control('add_cost_id', ['class'=>'form-control', 'options' => $addCosts]);
+                                    ?>
+                                </div>
+
                             </fieldset>
 
-                            <?= $this->Form->button(__('Submit')) ?>
+                            <?= $this->Form->button(__('Submit'), ['class' => 'btn btn-primary']) ?>
                             <?= $this->Form->end() ?>
                         </div>
                     </div>
                 </div>
             </div>
         </main>
-
     </div>
 </div>
-
+</body>
