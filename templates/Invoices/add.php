00d4eb5a9ded4fd1997ecc82e7d2cb5a2ef4b787
yzou01 <yzou0013@student.monash.edu>
2022-08-15 00:15:06 +1000

    fix merge clash on invoice add page

--- templates/Invoices/add.php
+++ templates/Invoices/add.php
@@ -7,47 +7,54 @@
 $key = isset($key) ? $key : '<%= key %>';
 ?>
 <body class="sb-nav-fixed">
-<?php echo $this->element('navbar/navbar')?>
-<div id="layoutSidenav">
-    <?php echo $this->element('navbar/sidebar')?>
-    <div id="layoutSidenav_content">
-        <main>
-            <br><br>
-            <div class=" card mb-4">
-                <div class="card-header">
-                    <i class="fas fa-table me-1" style="padding-top: 11px"></i>
-                    Add Bill Of Records
-                    <?= $this->Html->link(__('List Bill Of Records'), ['action' => 'index'], ['class' => 'btn btn-primary', 'style' => 'float: right; margin-right: 5px']) ?>
-                </div>
-                <div class="card-body">
-                    <div class="column-responsive column-80">
-                        <div class="skus form content">
-                            <?= $this->Form->create($invoice) ?>
-                            <fieldset>
-                                <div class="form-label">
-                                    <?php
-                                    echo $this->Form->control('number',['class'=>'form-control']);
-                                    ?>
-                                </div>
-                                <div class="form-label">
-                                    <?php
-                                    echo $this->Form->control('date',['class'=>'form-control']);
-                                    ?>
-                                </div>
-                                <div class="form-label">
-                                    <?php
-                                    echo $this->Form->control('factory_id',['class'=>'form-control']);
-                                    ?>
-                                </div>
-                                <div class="form-label">
-                                    <?php
-                                    echo $this->Form->control('currency_of_origin',['class'=>'form-control']);
-                                    ?>
-                                </div>
-                                <div class="form-label">
-                                    <?php
-                                    echo $this->Form->control('currency_rate',['class'=>'form-control', 'min'=>0]);
-                                    ?>
+    <?php echo $this->element('navbar/navbar')?>
+    <div id="layoutSidenav">
+        <?php echo $this->element('navbar/sidebar')?>
+        <div id="layoutSidenav_content">
+            <main>
+                <br><br>
+                <div class=" card mb-4">
+                    <div class="card-header">
+                        <i class="fas fa-table me-1" style="padding-top: 11px"></i>
+                        Add Bill Of Records
+                        <?= $this->Html->link(__('List Bill Of Records'), ['action' => 'index'], ['class' => 'btn btn-primary', 'style' => 'float: right; margin-right: 5px']) ?>
+                    </div>
+                    <div class="card-body">
+                        <div class="column-responsive column-80">
+<!--                            DETAILS COLUMN-->
+                            <div class="col-4" style="float: left">
+                                <h4>Details</h4>
+                                <div class=" form content">
+                                    <?= $this->Form->create($invoice) ?>
+                                    <fieldset>
+                                        <div class="form-label">
+                                            <?php
+                                            echo $this->Form->control('number',['class'=>'form-control']);
+                                            ?>
+                                        </div>
+                                        <div class="form-label">
+                                            <?php
+                                            echo $this->Form->control('date',['class'=>'form-control']);
+                                            ?>
+                                        </div>
+                                        <div class="form-label">
+                                            <?php
+                                            echo $this->Form->control('factory_id',['class'=>'form-control']);
+                                            ?>
+                                        </div>
+                                        <div class="form-label">
+                                            <?php
+                                            echo $this->Form->control('currency_of_origin',['class'=>'form-control']);
+                                            ?>
+                                        </div>
+                                        <div class="form-label">
+                                            <?php
+                                            echo $this->Form->control('currency_rate',['class'=>'form-control', 'min'=>0]);
+                                            ?>
+                                        </div>
+                                        
+                                    </fieldset>
+                                    
                                 </div>
                             </div>
 
@@ -166,6 +173,7 @@ $key = isset($key) ? $key : '<%= key %>';
 </body>
 
 <?php echo $this->Html->script(['underscore-min.js','additionalcosts.js'],['block'=>true]) ?>
+
 
 
 
