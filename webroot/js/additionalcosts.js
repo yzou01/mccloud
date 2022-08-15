5c8d1f24e78b667dffc1eb12f82e9f6ff58c11e9
Kai Quan Ang <kang0012@student.monash.edu>
2022-08-15 13:32:40 +1000

    Fixed dynamic form for items

--- webroot/js/additionalcosts.js
+++ webroot/js/additionalcosts.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-
+    //additionalCosts
     let additionalcostsContainer = $('#additionalcosts-container')
     let additionalcostsTemplate = _.template($('#additionalcosts-template').remove().text());
     let numbersRows = additionalcostsContainer.find('.additionalcosts-row').length;
@@ -13,6 +13,25 @@ $(document).ready(function () {
     })
 
     additionalcostsContainer.on('click','a.additionalcosts-delete',function (e){
+        e.preventDefault();
+        $(this).closest('.row').fadeOut('fast',function (){
+            $(this).remove()
+        })
+    })
+
+    //skus
+    let skusContainer = $('#skus-container')
+    let skusTemplate = _.template($('#skus-template').remove().text());
+    let numbersSkuRows = skusContainer.find('.skus-row').length;
+    let addSkusButton = $('#add-skus-button');
+
+    addSkusButton.on('click',function (e){
+        e.preventDefault();
+        $(skusTemplate({key: numbersSkuRows++})).hide().appendTo(skusContainer).fadeIn('fast')
+        skusTemplate[0].scrollTop = skusContainer[0].scrollHeight
+    })
+
+    skusContainer.on('click','a.skus-delete',function (e){
         e.preventDefault();
         $(this).closest('.row').fadeOut('fast',function (){
             $(this).remove()
