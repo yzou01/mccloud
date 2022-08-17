f7dd51321f2482e7e27450c4be3a492c3e107a69
Saurabh Iyer <siye0001@student.monash.edu>
2022-08-17 17:47:10 +1000

    Added Icon to assets

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -9,29 +9,35 @@
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>McCloud Admin</title>
-    <?= $this->Html->meta('icon') ?>
+
 
 
     <?= $this->fetch('meta') ?>
     <?= $this->fetch('css') ?>
-    
+
     <?= $this->Html->script(['jquery.min.js','jquery.easing.min.js','scripts.js']) ?>
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
         <?= $this->Html->css(['styles.css']) ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        
-        
+
+
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
             <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
-        
+
         <?= $this->Html->script('datatables-simple-demo.js') ?>
-        
+
 
         <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
 </head>
 <body>
-   
-    <main class="main">
+
+<?=$this->Html->meta(
+            'mccloud-shoes-melb-pty-ltd-melbourne-3000-billboard-large.jpg',
+            'Assets/mccloud-shoes-melb-pty-ltd-melbourne-3000-billboard-large.jpg',
+            ['type' => 'icon']
+            );?>
+
+<main class="main">
         <div class="container">
             <?= $this->Flash->render() ?>
             <?= $this->fetch('content') ?>
