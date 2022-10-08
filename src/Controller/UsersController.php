0662a7ea983e25cf3cda70dc3d836049c43efcbe
Saurabh Iyer <siye0001@student.monash.edu>
2022-10-08 15:21:13 +1100

    Forgot Password

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 
 namespace App\Controller;
-
+use Cake\Mailer\Mailer;
 /**
  * Users Controller
  *
@@ -48,7 +48,7 @@ class UsersController extends AppController
     {
         $user = $this->Users->newEmptyEntity();
         if ($this->request->is('post')) {
-            $user = $this->Users->patchEntity($user, $this->request->getData(),['validate'=>'passwords']);
+            $user = $this->Users->patchEntity($user, $this->request->getData(), ['validate' => 'passwords']);
             if ($this->Users->save($user)) {
                 $this->Flash->success(__('The user has been saved.'));
 
@@ -58,7 +58,6 @@ class UsersController extends AppController
         }
         $this->set(compact('user'));
     }
-
 
 
     /**
@@ -74,7 +73,7 @@ class UsersController extends AppController
             'contain' => [],
         ]);
         if ($this->request->is(['patch', 'post', 'put'])) {
-            $user = $this->Users->patchEntity($user, $this->request->getData(),['validate'=>'passwords']);
+            $user = $this->Users->patchEntity($user, $this->request->getData(), ['validate' => 'passwords']);
             if ($this->Users->save($user)) {
                 $this->Flash->success(__('The user has been saved.'));
 
@@ -110,7 +109,7 @@ class UsersController extends AppController
         parent::beforeFilter($event);
         // Configure the login action to not require authentication, preventing
         // the infinite redirect loop issue
-        $this->Authentication->addUnauthenticatedActions(['login','password']);
+        $this->Authentication->addUnauthenticatedActions(['login', 'password']);
     }
 
     public function login()
@@ -143,75 +142,11 @@ class UsersController extends AppController
         }
     }
 
-
     public function password(){
 
-        if($this->request->is('post'))
-        {
-            $user_data = $this->request->data;
-            if(!empty($user_data)){
-                $this->User->recursive=-1;
-                $check_email = $this->User->find('first',array('conditions'=>array('User.email_address'=>$user_data['User']['email_address'])));
-
-                if(!empty($check_email)){
-                    $data['id'] = $check_email['User']['id'];
-                    $characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-                    $new_password = '';
-                    for ($i=0; $i<6; $i++)
-                    {
-                        $new_password .= $characters[rand(0, strlen($characters) - 1)];
-                    }
-
-                    $data['password'] = md5($new_password);
-                    $this->User->save($data);
-                    /* Sending Email to user */
-                    $email=$user_data['User']['email_address'];
-                    $message = '';
-                    $message .= '<html>';
-                    $message.='<table style="width:800px;margin:auto;border-collapse:collapse;border:1px solid #5A5A5A;">';
-                    $message.='<thead style="background:#5A5A5A;">';
-                    $message.='<tr>';
-                    $message.='<td width="50%" style="padding:14px 20px;text-align:right;font-size:25px;color:#fff;"></td>';
-                    $message.='</tr>';
-                    $message.='</thead>';
-                    $message.='<tbody>';
-                    $message.='<tr>';
-                    $message.='<td style="padding:5px 20px;" colspan="2">';
-                    $message .= "<h3>New Password  :".$new_password."</h3></br>";
-
-
-                    $message .= '<br/><br/>Best Regards';
-                    $message .= '<br/><br/> My Team';
-                    $message.='</td>';
-                    $message.='</tr>';
-                    $message.='</tbody>';
-                    $message.='</table>';
-                    $message .= '<html>';
-                    $data_send['body'] = $message;
-                    $data_send['subject'] = "Forgot Password - My Team";
-
-                    $data_send['to'] = $email;
-                    //echo "<pre>";print_r($data_send);die;
-                    // echo "<pre>";print_r($data_send);die;
-
-                    $output = $this->send_mail($data_send);
-
-                    /* Sending Email to user */
-                    if($output){
-                        $this->Session->setFlash('Password has been changed, Check Your Mail', 'default', array('class' => 'example_class'));
-                        $this->redirect(array('controller' => 'users', 'action' => 'login'));
-                        //echo json_encode(array('status' => 'success', 'message' => "Password has been changed , please check your email")); die;
-                    }
-                    else{
-                        $this->Session->setFlash('Password has been changed ', 'default', array('class' => 'example_class'));
-                        $this->redirect(array('controller' => 'users', 'action' => 'registration'));
-                    }
-                }
-                else{
-                    $this->Session->setFlash('Email Not Exist', 'default', array('class' => 'example_class'));
-                    $this->redirect(array('controller' => 'users', 'action' => 'registration'));
-                }
-            }
-        }
     }
+
 }
+
+
+
--- templates/Users/password.php
+++ templates/Users/password.php
@@ -10,20 +10,10 @@
                         <div class="card shadow-lg border-0 rounded-lg mt-5">
                             <div class="card-header"><h3 class="text-center font-weight-light my-4">Password Recovery</h3></div>
                             <div class="card-body">
-                                <div class="small mb-3 text-muted">Enter your email address and we will send you your password.</div>
-                                <form>
-                                    <div class="form-floating mb-3">
-                                        <input class="form-control" id="inputEmail" type="email" placeholder="name@example.com" />
-                                        <label for="inputEmail">Email address</label>
-                                    </div>
-                                    <div class="form-label" style="margin-top: 20px; margin-left: 2px">
-                                        <?= $this->Html->link(__('Return to login'), ['action' => 'login']) ?>
-                                    </div>
-                                </form>
+                                <div class="small mb-3 text-muted">Please check Technical Documentation Section 3 for guidelines on how to regain access to the system.</div>
+
                             </div>
                             <div class="card-footer text-center py-3">
-                                <?= $this->Form->submit(__('Reset Password'), ['class'=>'btn btn-primary', 'style' => 'width: 100%']); ?>
-                                <?= $this->Form->end() ?>
                             </div>
                         </div>
                     </div>
